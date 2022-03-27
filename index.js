f3fc4478c1854e5f1ea80f419166edc0e1097734
the-eric-kwok <erickwok404@gmail.com>
2022-03-27 16:50:21 +0800

    增加了加减法题型

--- index.js
+++ index.js
@@ -23,15 +23,36 @@
         return [opr1, opr2];
     }
 
+    function plusAndSubGenerator() {
+        let opr1 = 0, opr2 = 0, opr3 = 0;
+        while (!(opr1 > 100 && opr1 < 999 && opr2 > 100 && opr2 < 999 && opr3 > 100 && opr3 < 999)) {
+            opr1 = parseInt(Math.random() * 1000);
+            opr2 = parseInt(Math.random() * 1000);
+            opr3 = parseInt(Math.random() * 1000);
+        }
+        return [opr1, opr2, opr3, Math.random() > 0.5 ? "+" : "-", Math.random() > 0.5 ? "+" : "-"];
+    }
+
     function refreshPuzzle() {
         if (document.querySelector('input[name="type"]:checked').value === "multiply") {
             let puzzle = multiplyPuzzleGenerator();
-            document.getElementById("puzzle").innerHTML = puzzle[0] + " × " + puzzle[1] + " =";
+            document.getElementById("puzzle").innerHTML = `${puzzle[0]} × ${puzzle[1]} = `;
             document.getElementById("answer").innerHTML = puzzle[0] * puzzle[1];
-        } else {
+        } else if (document.querySelector('input[name="type"]:checked').value === "obelus") {
             let puzzle = obelusPuzzleGenerator();
-            document.getElementById("puzzle").innerHTML = puzzle[0] + " ÷ " + puzzle[1] + " =";
+            document.getElementById("puzzle").innerHTML = `${puzzle[0]} ÷ ${puzzle[1]} = `;
             document.getElementById("answer").innerHTML = parseFloat((puzzle[0] / puzzle[1]).toFixed(1));
+        } else {
+            let puzzle = plusAndSubGenerator();
+            document.getElementById("puzzle").innerHTML = `${puzzle[0]} ${puzzle[3]} ${puzzle[1]} ${puzzle[4]} ${puzzle[2]} = `;
+            let answer = puzzle[0];
+            for (let i = 1; i < 3; i++) {
+                if (puzzle[i + 2] === "+")
+                    answer += puzzle[i];
+                else
+                    answer -= puzzle[i];
+            }
+            document.getElementById("answer").innerHTML = answer;
         }
     }
 
